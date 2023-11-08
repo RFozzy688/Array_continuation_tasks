8bf8c4ff95f5dc5d9dd767a0ed52ba969b4c8fb7
RFozzy688 <pokerfozzy@gmail.com>
2023-11-08 11:30:05 +0200

    fill/print array

---
+++ Array_continuation_tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_continuation_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            FillArray(array);
+
+            Console.WriteLine("Исходный массив:");
+            PrintArray(array);
+        }
+        static void FillArray(int[] arr)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 20);
+            }
+        }
+        static void PrintArray(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
